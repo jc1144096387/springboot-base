c9f0138cec6d7595fa3b9c488b26c572815a9be3
jc <1144096387@qq.com>
2020-02-17 11:53:24 +0800

    整合swagger。TODO：抽个时间专门学习swagger的详细内容，并写好总结文档

--- code/springboot-base/src/main/java/com/jc/springbootbase/controller/TestController.java
+++ code/springboot-base/src/main/java/com/jc/springbootbase/controller/TestController.java
@@ -1,13 +1,14 @@
 package com.jc.springbootbase.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // @RestController会自动将输出内容转化为JSON格式
 @RestController
 public class TestController {
-    // @RequestMapping("/test") 提供路由信息，"/test" 路径的 HTTP Request 都会被映射到 test() 方法上进行处理。
-    @RequestMapping("/test")
+    // @RequestMapping("/test") 提信息，"/test" 路径的 HTTP Request 都会被映射到 test() 方法上进行处理。
+    @GetMapping("/test")
     public String test(String name){
         return "test,你好， " + name;
     }
